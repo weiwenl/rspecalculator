cfb87a45cdc6b3510d5c81d461d49533e85a3569
weiwenl <weiwenl.justin@gmail.com>
2018-11-02 14:42:09 +0800

    classwork done up to power

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,2 +1,26 @@
 class Calculator
+
+  def add(a, b)
+    a + b
+  end
+
+  def sub(a,b)
+    a - b
+  end
+
+  def multi(a,b)
+    a * b
+  end
+
+  def div(a,b)
+    a / b
+  end
+
+  def pow(a,b)
+    a**b
+  end
+
+  def fac(a,b)
+
+  end
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -2,17 +2,44 @@ require 'spec_helper'
 require_relative '../lib/calc'
 
 describe Calculator do
-  describe '#add' do
-    it 'adds two positive numbers' do
-      expect(Calculator.new.add(2, 2)).to eq(4)
+
+    describe '#add' do
+      it 'adds two positive numbers' do
+        expect(Calculator.new.add(2, 2)).to eq(4)
+      end
+
+      it 'adds a negative and positive number' do
+        expect(Calculator.new.add(-1, 2)).to eq(1)
+      end
     end
 
-    it 'adds a negative and positive number' do
-      expect(Calculator.new.add(-1, 2)).to eq(1)
+    describe '#subtract' do
+      it 'subtracts two positive numbers' do
+      expect(Calculator.new.sub(10,7)).to eq(3)
+      end
     end
-  end
 
-  describe '#subtract' do
-    it 'subtracts two positive numbers'
-  end
+    describe '#multiply' do
+      it 'multiplies two positive numbers' do
+      expect(Calculator.new.multi(10,7)).to eq(70)
+      end
+    end
+
+    describe '#divide' do
+      it 'divide two positive numbers' do
+      expect(Calculator.new.div(9,3)).to eq(3)
+      end
+    end
+
+    describe '#power' do
+      it 'finds the sum of a number with a power value' do
+      expect(Calculator.new.pow(9,2)).to eq(81)
+      end
+    end
+
+    describe '#factorial' do
+      it 'finds the factorial of a number defined by the number of times' do
+      
+      end
+    end
 end
